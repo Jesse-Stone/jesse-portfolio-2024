a10b2026e718f4124cef0b9a138e7f65618ca0be
Jesse Stone <jessecharles@gmail.com>
2024-01-18 13:39:51 -0700

    cards and data

--- jesse-portfolio-2024/src/components/ProjectsList.jsx
+++ jesse-portfolio-2024/src/components/ProjectsList.jsx
@@ -1,34 +1,34 @@
 import "../App.css";
-import { Flex, Text } from "@radix-ui/themes";
+import { Box, Card, Flex, Text } from "@radix-ui/themes";
+import ProjectListData from "../data/ProjectsListData";
 
 function ProjectsList() {
   return (
     <>
-      <Flex direction={"column"}>
-        <Text size={"9"}>ONE</Text>
-        <Text size={"9"}>ONE</Text>
-        <Text size={"9"}>ONE</Text>
-        <Text size={"9"}>ONE</Text>
-        <Text size={"9"}>ONE</Text>
-        <Text size={"9"}>ONE</Text>
-        <Text size={"9"}>ONE</Text>
-        <Text size={"9"}>ONE</Text>
-        <Text size={"9"}>ONE</Text>
-        <Text size={"9"}>ONE</Text>
-        <Text size={"9"}>ONE</Text>
-        <Text size={"9"}>ONE</Text>
-        <Text size={"9"}>ONE</Text>
-        <Text size={"9"}>ONE</Text>
-        <Text size={"9"}>ONE</Text>
-        <Text size={"9"}>ONE</Text>
-        <Text size={"9"}>ONE</Text>
-        <Text size={"9"}>ONE</Text>
-        <Text size={"9"}>ONE</Text>
-        <Text size={"9"}>ONE</Text>
-        <Text size={"9"}>ONE</Text>
-        <Text size={"9"}>ONE</Text>
-        <Text size={"9"}>ONE</Text>
-        <Text size={"9"}>ONE</Text>
+      <Flex direction={"column"} gap={"3"}>
+        {ProjectListData.map((project) => (
+          <Card
+            style={{ maxWidth: 500, borderColor: "grey", padding: "10px" }}
+            key={project.index}
+          >
+            <Flex direction={"column"} gap={"2"}>
+              <Text
+                size={"1"}
+                style={{
+                  fontWeight: "900",
+                  letterSpacing: "4px",
+                  fontFamily: ["Josefin Sans", "sans-serif"],
+                }}
+              >
+                {project.languages.toUpperCase()}
+              </Text>
+              <Text size={"6"} weight={"bold"}>
+                {project.name}
+              </Text>
+              <Text>{project.description}</Text>
+            </Flex>
+          </Card>
+        ))}
       </Flex>
     </>
   );
---
+++ jesse-portfolio-2024/src/data/ProjectsListData.jsx
@@ -0,0 +1,59 @@
+const ProjectListData = [
+  {
+    name: "Lunar Pooper",
+    languages: "C#",
+    description: "I distilled farts using AI/ML",
+  },
+  {
+    name: "Second Project",
+    languages: "Typescript",
+    description: "LLM that measures weiner sizes",
+  },
+  {
+    name: "Third Project",
+    languages: "Typescript",
+    description: "Game where you pee as far as you can",
+  },
+  {
+    name: "Third Project",
+    languages: "Python",
+    description: "Game where you pee as far as you can",
+  },
+  {
+    name: "Third Project",
+    languages: "Javascript",
+    description: "Game where you pee as far as you can",
+  },
+  {
+    name: "Third Project",
+    languages: "Python / Unity",
+    description: "Game where you pee as far as you can",
+  },
+  {
+    name: "Third Project",
+    languages: "Javascript",
+    description: "Game where you pee as far as you can",
+  },
+  {
+    name: "Third Project",
+    languages: "Python / Unity",
+    description: "Game where you pee as far as you can",
+  },
+  {
+    name: "Third Project",
+    languages: "Python / Unity",
+    description: "Game where you pee as far as you can",
+  },
+  {
+    name: "Third Project",
+    languages: "Python / Unity",
+    description: "Game where you pee as far as you can",
+  },
+  {
+    name: "Third Project",
+    languages: "Python / Unity",
+    description: "Game where you pee as far as you can",
+  },
+];
+
+export default ProjectListData;
