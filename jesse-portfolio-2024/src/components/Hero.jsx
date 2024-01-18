401b46ce479ff9983f9fecb7c52f2c59f04b60e2
Jesse Stone <jessecharles@gmail.com>
2024-01-18 00:50:43 -0700

    two col layout test

--- jesse-portfolio-2024/src/components/Hero.jsx
+++ jesse-portfolio-2024/src/components/Hero.jsx
@@ -1,50 +1,61 @@
 import "../App.css";
 import * as Avatar from "@radix-ui/react-avatar";
-import { motion } from "framer-motion";
-import { Container, Flex, Text } from "@radix-ui/themes";
-import { GitHubLogoIcon, LinkedInLogoIcon } from "@radix-ui/react-icons";
+import { Box, Flex, Text } from "@radix-ui/themes";
+import {
+  ExternalLinkIcon,
+  GitHubLogoIcon,
+  LinkedInLogoIcon,
+} from "@radix-ui/react-icons";
 import jesseAvatar from "../assets/JesseAvatar.jpeg";
 
 function Hero() {
   return (
     <>
-      <Container size={"3"}>
-        <Flex justify={"center"} direction={"column"} align={"center"}>
-          <Avatar.Root className='AvatarRoot'>
-            <Avatar.Image
-              className='AvatarImage'
-              src={jesseAvatar}
-            ></Avatar.Image>
-          </Avatar.Root>
-
-          <Text size={"9"} align={"center"} style={{ fontWeight: "900" }}>
-            Experienced web developer specializing in{" "}
-            <span className='Gradient'>ReactJS</span>
-            <span>.</span>
+      <Flex>
+        <Box mr={"auto"} style={{ maxWidth: "700px", position: "sticky" }}>
+          <Text className="Hero__Name" size={"9"}>
+            Jesse Stone
           </Text>
-          <Text
-            size={"5"}
-            align={"center"}
-            weight={"light"}
-            mt={"4"}
-            style={{ opacity: "90%" }}
-          >
-            Some bullshit about how great I am, all the made up crap I can do
-            and have done, and also how big my yammer is - I will deliver
-            results with said yammer that I mentioned preveriously.
-          </Text>
-
-          <Flex
-            justify={"space-between"}
-            align={"center"}
-            mt={"3"}
-            width={"50%"}
-          >
-            <GitHubLogoIcon style={{ height: "35px", width: "35px" }} />
-            <LinkedInLogoIcon style={{ height: "35px", width: "35px" }} />
+          <Flex direction={"column"}>
+            <Text size={"9"} mt={"2"} style={{ fontWeight: "300" }}>
+              Experienced web developer specializing in{" "}
+              <span className="Gradient">ReactJS</span>
+              <span>.</span>
+            </Text>
+            <Text
+              size={"5"}
+              weight={"light"}
+              mt={"4"}
+              style={{ opacity: "90%" }}
+            >
+              Some bullshit about how great I am, all the made up crap I can do
+              and have done, and also how big my yammer is - I will deliver
+              results with said yammer that I mentioned preveriously.
+            </Text>
           </Flex>
-        </Flex>
-      </Container>
+          <Flex direction={"row"} align={"center"} gap={"4"} mt={"3"}>
+            <Avatar.Root className="AvatarRoot">
+              <Avatar.Image
+                className="AvatarImage"
+                src={jesseAvatar}
+              ></Avatar.Image>
+            </Avatar.Root>
+            <Flex align={"center"} gap={"5"}>
+              <Flex align={"center"} gap={"2"}>
+                <GitHubLogoIcon style={{ height: "20px", width: "20px" }} />
+                <Text>Github</Text>
+                <ExternalLinkIcon />
+              </Flex>
+              <Flex align={"center"} gap={"2"}>
+                <LinkedInLogoIcon style={{ height: "20px", width: "20px" }} />
+                <Text>LinkedIn</Text>
+                <ExternalLinkIcon />
+              </Flex>
+            </Flex>
+          </Flex>
+        </Box>
+        <Box>TEST</Box>
+      </Flex>
     </>
   );
 }
